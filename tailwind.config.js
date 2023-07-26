47479934dafaaf53ac01811c7595ea43b337bd1f
ProgramerNila <programernila@gmail.com>
2023-07-26 11:47:55 +0600

    navbar

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F85559",
+        secondary: "rgba(18, 18, 18, 0.6)",
+        headingColor: "#121212",
+      },
+    },
+
+  },
+  plugins: [],
+};
